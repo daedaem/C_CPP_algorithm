3052f4b244aa0abcce608e7d02601d39ab494c72
tg8685 <tg8685@gmail.com>
2022-07-17 01:12:55 +0900

    Docs: update 배열2_형성평가7

--- JUNGOL/배열2/형성평가7.c
+++ JUNGOL/배열2/형성평가7.c
@@ -1,5 +1,23 @@
 #include <stdio.h>
 
 int main(){
-    
+    int arr1[2][3],arr2[2][3], r,c;
+    printf("first array\n");
+    for(r=0; r<2;r++){
+        for(c=0; c<3;c++){
+            scanf("%d", &arr1[r][c]);
+        }
+    }
+    printf("second array\n");
+    for(r=0; r<2;r++){
+        for(c=0; c<3;c++){
+            scanf("%d", &arr2[r][c]);
+        }
+    }
+    for(r=0; r<2;r++){
+        for(c=0; c<3;c++){
+            printf("%d ", arr1[r][c]*arr2[r][c]);
+        }
+        printf("\n");
+    }
 }
